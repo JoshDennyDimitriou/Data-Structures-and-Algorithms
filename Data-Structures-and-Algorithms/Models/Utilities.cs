9825790a6152e8f92bfcf630f7b90f70b661f6a1
Josh Denny-Dimitriou <joshua.denny-dimitriou@student.tafesa.edu.au>
2026-03-05 14:56:27 +1030

    Added a Utilities class, coded a linear search and binary search algorithms

---
+++ Data-Structures-and-Algorithms/Models/Utilities.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_and_Algorithms.Models
+{
+    class Utilities
+    {
+        // Linear Search Algorithm
+        static int LinearSearch<T>(T[]myArray, T criteria) where T : IComparable<T>
+        {
+            int i = 0;
+            bool found = false;
+            while (!found && i < myArray.Length)
+            {
+                if (criteria.CompareTo(myArray[i]) == 0)
+                    found = true;
+                else
+                    i++;
+            }
+            if (i < myArray.Length)
+                return i;
+            else
+                return -1;
+        }
+
+        // Binary Search Algorithm
+        static int BinarySearch<T>(T[] myArray, T criteria) where T : IComparable<T>
+        {
+            int min = 0;
+            int max = myArray.Length - 1;
+            int mid;
+
+            do
+            {
+                mid = (min + max) / 2;
+                if (myArray[mid].CompareTo(criteria) == 0)
+                    return mid;
+                if (criteria.CompareTo(myArray[mid]) > 0)
+                    min = mid + 1;
+                else
+                    max = mid - 1;
+            } while (mid <= max);
+            return - 1;
+        }
+    }
+}
