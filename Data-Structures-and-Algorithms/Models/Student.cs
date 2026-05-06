fb8ac6e499268d236a34d95a3b11469456f6785a
Josh Denny-Dimitriou <joshdennyd@gmail.com>
2026-05-06 20:04:21 +0930

    Replaced the student.cs ToString method with the correct formatting

--- Data-Structures-and-Algorithms/Models/Student.cs
+++ Data-Structures-and-Algorithms/Models/Student.cs
@@ -80,18 +80,18 @@ namespace Data_Structures_and_Algorithms
         /// An override of the default ToString method
         /// </summary>
         /// <returns>string</returns>
-        //public override string ToString()
-        //{
-        //    return base.ToString() + "Student ID: " + StudentStudentID +
-        //        "\nProgram: " + StudentProgram +
-        //        "\nDate Registered:" + StudentDateRegistered +
-        //        "\nEnrollment: [\n" + StudentEnrollment + "\n\t\t]";
-        //}
-
         public override string ToString()
         {
-            return "Student ID: " + StudentStudentID + "\n";
+            return base.ToString() + "Student ID: " + StudentStudentID +
+                "\nProgram: " + StudentProgram +
+                "\nDate Registered:" + StudentDateRegistered +
+                "\nEnrollment: [\n" + StudentEnrollment + "\n\t\t]";
         }
+
+        //public override string ToString()
+        //{
+        //    return "Student ID: " + StudentStudentID + "\n";
+        //}
 
         /// <summary>
         /// An override of the default Equals method
